c62bc8b1f9fa3bc9b374e005392eea6ba0f93d72
Michael Cereda <michael.cereda@gmail.com>
2016-05-06 12:07:04 -0400

    Create README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# michaelcereda.com
+Source code of my website (michaelcereda.com)
+
+#License
+Everything except "content files" (Images, md/text files containing  informations, etc) can be used for learning purposes.
---
+++ test.php
@@ -0,0 +1,3 @@
+~/scripts/domain_init.sh -u soims4s -g soims4s -p /srv/products/shiverstudio/ipim.it -h ipim_it -d ipim.it -f ipim_it
+~/scripts/domain_init.sh -u soims4s -g soims4s -p /srv/products/shiverstudio/metalelectric.it -h metalelectric_it -d metalelectric.it -f metalelectric_it
+~/scripts/domain_init.sh -u soims4s -g soims4s -p /srv/products/shiverstudio/wela.it -h wela_it -d wela.it -f wela_it
